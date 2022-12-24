b4caa478a85bf39f095266a68be03a7ef0d1553b
Max <maxigor.ferreira@gmail.com>
2022-12-23 21:02:33 -0300

    miss type

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -79,7 +79,7 @@ echo -ne "
 # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
 # stop the script and move on, not installing any more packages below that line
 if [[ ! $DESKTOP_ENV == server ]]; then
-  sed -n '/'$INSTALL_TYPE'/q;p' $HOME/nabucodonosor/pkg-files/pacman-pkgs.txt | while read line
+  sed -n '/'$INSTALL_TYPE'/q;p' $HOME/nabucodonosor/pkg-files/pacman-pkg.txt | while read line
   do
     if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
       # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
