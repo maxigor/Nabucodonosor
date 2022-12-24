6d1f278ece8fab5e550a00c17222309514bbad0e
Max <maxigor.ferreira@gmail.com>
2022-12-24 11:06:23 -0300

    nvim nvchad cfg

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -1,16 +1,34 @@
-require("max.plugins-setup")
-require("max.core.options")
-require("max.core.keymaps")
-require("max.core.colorscheme")
-require("max.plugins.comment")
-require("max.plugins.nvim-tree")
-require("max.plugins.lualine")
-require("max.plugins.telescope")
-require("max.plugins.nvim-cmp")
-require("max.plugins.lsp.mason")
-require("max.plugins.lsp.lspsaga")
-require("max.plugins.lsp.lspconfig")
-require("max.plugins.lsp.null-ls")
-require("max.plugins.autopairs")
-require("max.plugins.treesitter")
-require("max.plugins.gitsigns")
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
+
+require "core"
+require "core.options"
+
+-- setup packer + plugins
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
+end
+
+pcall(require, "custom")
+
+require("core.utils").load_mappings()
