2b01fad740f61b71196065e52c39f7d5bfd66512
max <maxigor.ferreira@gmail.com>
2023-08-04 18:05:35 -0300

    so many new f things

--- configs/.config/bspwm/autostart.sh
+++ configs/.config/bspwm/autostart.sh
@@ -3,7 +3,13 @@
 picom &
 xautolock -time 5 -locker i3lock-fancy &
 alacritty &
+nordvpn c
 firefox &
 thunderbird &
 qbittorrent &
 insync start &
+telegram &
+hexchat &
+discord &
+spotify-launcher &
+steam &
