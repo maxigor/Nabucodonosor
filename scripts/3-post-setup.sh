47c3a82eda53be701245abd7eadeae1670c3722d
Max <maxigor.ferreira@gmail.com>
2024-04-09 15:06:49 -0300

    oh noes joes

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -24,7 +24,7 @@ GRUB EFI Bootloader Install & Check
 source ${HOME}/nabucodonosor/configs/setup.conf
 
 if [[ -d "/sys/firmware/efi" ]]; then
-    grub-install --efi-directory=/boot ${DISK}
+	grub-install --efi-directory=/boot ${DISK}
 fi
 
 echo -ne "
@@ -33,32 +33,29 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 # set kernel parameter for decrypting the drive
-if [[ "${FS}" == "luks" ]]; then
-sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
-fi
+#if [[ "${FS}" == "luks" ]]; then
+#sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
+#fi
 # set kernel parameter for adding splash screen
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
+#sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
 
-echo -e "Installing CyberRe Grub theme..."
-THEME_DIR="/boot/grub/themes"
-THEME_NAME=CyberRe
-echo -e "Creating the theme directory..."
-mkdir -p "${THEME_DIR}/${THEME_NAME}"
-echo -e "Copying the theme.. e."
-cd ${HOME}/nabucodonosor
-cp -a configs${THEME_DIR}/${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
-echo -e "Backing up Grub config..."
-cp -an /etc/default/grub /etc/default/grub.bak
-echo -e "Setting the theme as the default..."
-grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
-echo -e "Updating grub..."
-cp -r ${HOME}/nabucodonosor/configs/default/grub /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-echo -e "All set!"
-
-
-
+#echo -e "Installing CyberRe Grub theme..."
+#THEME_DIR="/boot/grub/themes"
+#THEME_NAME=CyberRe
+#echo -e "Creating the theme directory..."
+#mkdir -p "${THEME_DIR}/${THEME_NAME}"
+#echo -e "Copying the theme.. e."
+#cd ${HOME}/nabucodonosor
+#cp -a configs${THEME_DIR}/${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
+#echo -e "Backing up Grub config..."
+#cp -an /etc/default/grub /etc/default/grub.bak
+#echo -e "Setting the theme as the default..."
+#grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
+#echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
+#echo -e "Updating grub..."
+#cp -r ${HOME}/nabucodonosor/configs/default/grub /etc/default/grub
+#grub-mkconfig -o /boot/grub/grub.cfg
+#echo -e "All set!"
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -132,7 +129,6 @@ echo "Enabling and Starting Plex Media Service..."
 sudo systemctl enable plexmediaserver
 sudo systemctl start plexmediaserver
 
-
 echo "enabling virtualbox service"
 sudo systemctl enable vboxservice.service
 
@@ -170,23 +166,22 @@ git config --global user.name Max
 
 echo "done"
 
-
 echo -ne "
 -------------------------------------------------------------------------
                Enabling (and Theming) Plymouth Boot Splash
 -------------------------------------------------------------------------
 "
-PLYMOUTH_THEMES_DIR="$HOME/nabucodonosor/configs/usr/share/plymouth/themes"
-PLYMOUTH_THEME="hexagon_red" # can grab from config later if we allow selection
-mkdir -p /usr/share/plymouth/themes
-echo 'Installing Plymouth theme...'
+#PLYMOUTH_THEMES_DIR="$HOME/nabucodonosor/configs/usr/share/plymouth/themes"
+#PLYMOUTH_THEME="hexagon_red" # can grab from config later if we allow selection
+#mkdir -p /usr/share/plymouth/themes
+#echo 'Installing Plymouth theme...'
 
-cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
+#cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
 
-sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+#sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
 
-plymouth-set-default-theme -R hexagon_red # sets the theme and runs mkinitcpio
-echo 'Plymouth theme installed'
+#plymouth-set-default-theme -R hexagon_red # sets the theme and runs mkinitcpio
+#echo 'Plymouth theme installed'
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -194,10 +189,10 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-  sed -i 's/MODULE=(*/& nvidia nvidia_modset nvidia_uvm nvidia_drm/' /etc/mkinitcpio.conf # add nvidia modules 
-  cp {HOME}/nabucodonosor/configs/70-nvidia.rules /etc/udev/rules.d/
-  mkinitcpio -P
+if grep -E "NVIDIA|GeForce" <<<${gpu_type}; then
+	sed -i 's/MODULE=(*/& nvidia nvidia_modset nvidia_uvm nvidia_drm/' /etc/mkinitcpio.conf # add nvidia modules
+	cp {HOME}/nabucodonosor/configs/70-nvidia.rules /etc/udev/rules.d/
+	mkinitcpio -P
 fi
 echo -ne "
 -------------------------------------------------------------------------
