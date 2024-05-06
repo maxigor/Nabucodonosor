bb765c15e44ddbff267acf959dc6db2eca53b9f4
Max <maxigor.ferreira@gmail.com>
2024-05-06 17:02:01 -0300

    bruh bruuuuhh

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -113,15 +113,15 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 # Graphics Drivers find and install
-gpu_type=$(lspci)
+#gpu_type=$(lspci)
 #if grep -E "NVIDIA|GeForce" <<<${gpu_type}; then
 #	pacman -S --noconfirm --needed nvidia-lts nvidia-utils nvidia-settings
 #	nvidia-xconfig
-elif grep -E "Integrated Graphics Controller" <<<${gpu_type}; then
-	pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-elif grep -E "Intel Corporation UHD" <<<${gpu_type}; then
-	pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
-fi
+#elif grep -E "Integrated Graphics Controller" <<<${gpu_type}; then
+#	pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+#elif grep -E "Intel Corporation UHD" <<<${gpu_type}; then
+#	pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+#fi
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -171,7 +171,7 @@ echo -ne "
 "
 if [ $(whoami) = "root" ]; then
 	groupadd libvirt
-	useradd -m -G wheel,libvirt -s /bin/zsh $USERNAME
+	useradd -m -G wheel,libvirt -s /bin/bash $USERNAME
 	echo "$USERNAME created, home directory created, added to wheel and libvirt group, default shell set to /bin/bash"
 
 	# use chpasswd to enter $USERNAME:$password
